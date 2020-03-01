a5bf84a3ed02f70094c7126a437a4d3cef6ff3f5
Kateryna Strough <katerynastrough@hotmail.com>
2020-03-01 00:29:23 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -7,4 +7,17 @@ import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
 import App from './App';
 import './index.css';
 
+
+const store = createStore(
+  shoppingListItemReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App store={store} />
+  </Provider>,
+  document.getElementById('root')
+);
+
 ReactDOM.render(<App />, document.getElementById('root'));
